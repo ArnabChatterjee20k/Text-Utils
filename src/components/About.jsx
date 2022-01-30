8fbd240ea3cced3c9538a547b955f0a02743baee
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-01-30 09:37:36 +0530

    fixed accordion item by adding id to collapseTwo , modified textform counting algo and added capitalise function to alert

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,11 +25,11 @@ export default function About(props) {
             
             <div className="accordion-item">
                 <h2 className="accordion-header" id="headingOne">
-                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style={myStyle}>
+                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseOne" style={myStyle}>
                         About the Creator
                     </button>
                 </h2>
-                <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div id="collapseTwo" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                     <div className="accordion-body" style={myStyle}>
                         This is made by <strong>Arnab Chatterjee </strong>.
                     </div>
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export default function TextForm(props) {
@@ -8,46 +8,53 @@ export default function TextForm(props) {
 
     const handle_uppercase = () => {
         setText(text.toUpperCase())
-        props.render_alert("success","Converted to Uppercase")
+        props.render_alert("success", "Converted to Uppercase")
     }
-    
+
     const handle_lowercase = () => {
         setText(text.toLowerCase())
-        props.render_alert("success","Converted to Lowercase")
+        props.render_alert("success", "Converted to Lowercase")
     }
-    
+
     const calc = () => {
         let check = /^[^\sa-z_]+$/i
         if (check.test(text)) {
             setText(String(eval(text))) // string since trim will give error in case if text not string. eval returns a Number
-            props.render_alert("success","Calculation Done")
+            props.render_alert("success", "Calculation Done")
         }
         else {
-            props.render_alert("danger","Enter valid characters")
+            props.render_alert("danger", "Enter valid characters")
         }
     }
-    
-    const remove_extra_spaces = ()=>{
+
+    const remove_extra_spaces = () => {
         let new_text = text.split(/[ ]+/); // split by getting one or more spaces. + due to one or more spaces
         setText(new_text.join(" ")) // joining all words by one space
-        props.render_alert("success","Extra spaces removed")
+        props.render_alert("success", "Extra spaces removed")
     }
-    
-    const handle_copy = ()=>{
+
+    const handle_copy = () => {
         navigator.clipboard.writeText(text)
-        props.render_alert("success","Copied to Clipboard")
-    } 
+        props.render_alert("success", "Copied to Clipboard")
+    }
 
     // this must be present at the top level of the functional component before using them.
     // const default_val = "Enter text here";
     // const [text , setText] = useState()
     const [text, setText] = useState("") // array destructuring
+
+    // to disabled btns if no letter is present in the textbox
+    useEffect(() => {
+        let btns = document.querySelectorAll(".btn")
+        text.length===0?btns.forEach(e=>e.disabled = true):btns.forEach(e=>{e.disabled=false})
+    }, [text]);
+
     return (
         <>
-            <div className="container my-3 py-3"  style={{color : props.mode==="dark"?"white":"black"}}>
+            <div className="container my-3 py-3" style={{ color: props.mode === "dark" ? "white" : "black" }}>
                 <h2 className='my-3'>{props.heading}</h2>
                 <div className="mb-3">
-                    <textarea className="form-control p-3" name="text" id="text" rows="8" placeholder='Enter Your Text..' value={text} onChange={handle_change} style={{backgroundColor : props.body_col,color:props.mode==="dark"?"white":"black"}}></textarea>
+                    <textarea className="form-control p-3" name="text" id="text" rows="8" placeholder='Enter Your Text..' value={text} onChange={handle_change} style={{ backgroundColor: props.body_col, color: props.mode === "dark" ? "white" : "black" }}></textarea>
                 </div>
                 <button className="btn btn-primary m-2" onClick={handle_uppercase}>Convert To UPPERCASE</button>
                 <button className="btn btn-primary m-2" onClick={handle_lowercase}>Convert To lowercase</button>
@@ -56,25 +63,27 @@ export default function TextForm(props) {
                 <button className="btn btn-success m-2" onClick={handle_copy}>Copy Text</button>
                 <button className="btn btn-danger m-2" onClick={() => setText("")}>Reset</button>
             </div>
-            <div className="container my-3 py-3" style={{color : props.mode==="dark"?"white":"black"}}>
+            <div className="container my-3 py-3" style={{ color: props.mode === "dark" ? "white" : "black" }}>
                 <h3>Your Text Summary</h3>
                 <ul>
                     <li>
-                        <b className="text-warning" style={{fontSize: "1.35rem" }}>{text.trim() === "" ? 0 : text.split(" ").length}</b> words
+                        {/* word means split by space. character means split every letter*/}
+                        <b className="text-warning" style={{ fontSize: "1.35rem" }}>{text.length > 0 ? text.split(" ").filter(e => e.length !== 0).length : 0}</b> words
                     </li>
 
                     <li>
-                        <b className="text-warning" style={{fontSize: "1.35rem" }}>{text.trim() === "" ? 0 : text.length}</b> characters
+                        <b className="text-warning" style={{ fontSize: "1.35rem" }}>{text.length > 0 ? text.split("").filter(e => e !== " ").length : 0}</b> characters
                     </li>
 
                     <li>
-                        <b className="text-warning" style={{fontSize: "1.35rem" }}>{text.trim() === "" ? 0 : 1 / 125 * text.length}</b> Minutes reading time
+                        {/* divding the output of the condition(total characters without any spaces) by 125 */}
+                        <b className="text-warning" style={{ fontSize: "1.35rem" }}>{(text.length > 0 ? text.split("").filter(e => e !== " ").length : 0) / 125}</b> Minutes reading time
                     </li>
-                    
+
                 </ul>
                 <div className='p-1'>
                     <h3>Preview</h3>
-                    <p>{text.length>0?text:"Enter text to preview it."}</p>
+                    <p>{text.length > 0 ? text : "Enter text to preview it."}</p>
                 </div>
             </div>
         </>
